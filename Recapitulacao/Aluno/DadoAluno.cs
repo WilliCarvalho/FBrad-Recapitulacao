b6db7f49e22d8302fbae7046c4f9aedae2d0fcad
Willian Carvalho <50246022+WilliCarvalho@users.noreply.github.com>
2024-02-01 20:44:31 -0300

    Store student data system created

---
+++ Recapitulacao/Aluno/DadoAluno.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+class DadoAluno
+{
+    public string nome;
+    public int idade;
+    public string curso;
+    public float nota;
+
+
+    public DadoAluno() { }
+
+    public DadoAluno(string nome, int idade, string curso, float nota)
+    {
+        this.nome = nome;
+        this.idade = idade;
+        this.curso = curso;
+        this.nota = nota;
+    }
+
+    public string RetornaDadosDoAluno()
+    {
+        return "nome: " + nome + 
+               ", idade: " + idade + 
+               ", curso: " + curso + 
+               ", nota: " + nota;
+    }
+}
+
---
+++ Recapitulacao/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Recapitulacao
+{
+    class Program
+    {
+        static DadoAluno[] alunos;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos alunos sua sala possui? ");
+            int numeroAlunos = Int32.Parse(Console.ReadLine());
+            alunos = new DadoAluno[numeroAlunos];
+
+            for (int i = 0; i < alunos.Length; i++)
+            {
+                Print("Dados do aluno " + (i + 1));
+                Console.Write("Qual o nome do seu aluno? ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Qual a idade do seu aluno? ");
+                int idade = int.Parse(Console.ReadLine());
+
+                Console.Write("Qual o curso do seu aluno? ");
+                string curso = Console.ReadLine();
+
+                Console.Write("Qual a nota do seu aluno? ");
+                float nota = float.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                alunos[i] = new DadoAluno(nome, idade, curso, nota);
+            }
+
+            foreach (DadoAluno aluno in alunos)
+            {
+                Print(aluno.RetornaDadosDoAluno());
+            }
+        }
+
+        static void Print(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
